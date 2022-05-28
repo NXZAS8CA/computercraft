acccdd9dc2d38875111fd163f162a2663e92a69e
NXZAS8CA <simon.krieger@mailbox.org>
2022-05-28 12:50:01 +0200

    i dunno what i have done yesterday in the night

--- treefarm.lua
+++ treefarm.lua
@@ -8,6 +8,7 @@ function checkFuel()
 end
 
 function checkTree()
+    print("Checking...")
     local success, data = turtle.inspect()
     if data.name == "minecraft:brich_log" then
         print("Tree detected")
@@ -17,12 +18,13 @@ function checkTree()
 end
 
 function checkSapling()
+    print("Checking...")
     local success, data = turtle.inspect()
     if data.name == "minecraft:birch_sapling" then
         print("Sapling detected")
         return true
-    else return false 
     end
+    return false
 end
 
 function chopTree()
@@ -34,7 +36,7 @@ function chopTree()
         turtle.up()
     end
     while turtle.detectDown() == false do
-        tur.down()
+        turtle.down()
     end
     turtle.back()
 end
@@ -44,38 +46,6 @@ function setSapling()
     turtle.select(3)
     turtle.place()
     turtle.select(1)
-end
-
-function relocateTurtle()
-    checkFuel()
-    turtle.forward()
-    turtle.turnRight()
-    switchLines(4)
-    turtle.turnRight()
-    checkFuel()
-end
-
-function switchLines(count)
-    for iter = 1, count do
-        turtle.forward()
-    end
-end
-
-function transferInventory()
-    print("Dropping inventory")
-    for inventory = 2,9 do
-        turtle.select(inventory)
-        turtle.drop()
-    end
-end
-
-function moveToChest()
-    turtle.forward()
-    turtle.turnLeft()
-    for iter = 1, 7 do
-        turtle.forward()
-    end
-    turtle.turnRight()
 end
 
 function tasks()
@@ -89,46 +59,42 @@ function tasks()
     checkFuel()
 end
 
+function relocateTurtle()
+    checkFuel()
+    turtle.forward()
+    turtle.turnRight()
+    switchLines()
+    turtle.turnRight()
+    checkFuel()
+end
+
+function switchLines(count)
+    for iter = 1,count do
+        turtle.forward()
+    end
+end
+
 local depth = 15
-function runner()
+local chopping = true
+while chopping do
     turtle.forward()
     depth = depth - 1
-    
+
     turtle.turnLeft()
-    
-    if checkTree() then
-        chopTree()
-    end
-    --tasks()
-    
+    tasks()
+
     turtle.turnRight()
     turtle.turnRight()
     tasks()
-    
+
     turtle.turnLeft()
+
+    if depth == 0 then
+        relocateTurtle()
+        depth = 15
+    end
 end
 
-local chopping = true
-while chopping do
-    runner()
-    
-    
-    --while depth > 0 do
-    --    runner()
-    --end
-
-    --if depth == 0 then
-    --    relocateTurtle()
-    --    depth = 15
-    --end
-
-    --while depth > 0 do
-    --    runner()
-    --end
-    --moveToChest()
-    --transferInventory()
-    --chopping = false
-end
 
 
 
